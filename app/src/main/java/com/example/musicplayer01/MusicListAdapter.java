3253d1a774e5d99ba88fa6e9adcf2f477387d16d
evasrevird <45438031+evasrevird@users.noreply.github.com>
2023-06-17 11:26:07 +0700

    01

---
+++ app/src/main/java/com/example/musicplayer01/MusicListAdapter.java
@@ -0,0 +1,66 @@
+package com.example.musicplayer01;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MusicListAdapter extends RecyclerView.Adapter<MusicListAdapter.ViewHolder>{
+    ArrayList<AudioModel> songList;
+    Context context;
+
+    public MusicListAdapter(ArrayList<AudioModel> songList, Context context) {
+        this.songList = songList;
+        this.context = context;
+    }
+
+    public ArrayList<AudioModel> getSongList() {
+        return songList;
+    }
+
+    public void setSongList(ArrayList<AudioModel> songList) {
+        this.songList = songList;
+    }
+
+    public Context getContext() {
+        return context;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.recycler_item,parent,false);
+        return new MusicListAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        AudioModel songData = songList.get(position);
+        holder.titleTextView.setText(songData.getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return songList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView titleTextView;
+        ImageView iconImageView;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            titleTextView = itemView.findViewById(R.id.music_title);
+            iconImageView = itemView.findViewById(R.id.icon_view);
+        }
+    }
+}
